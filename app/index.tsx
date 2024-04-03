7df1b2f0ab4d9c409ba2b1d0d13a9c2db92ac7d0
Ev357 <evest.dev@gmail.com>
2024-04-03 23:39:50 +0200

    build: some basic expo router setup

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text, Platform } from "react-native";
+import "@/assets/css/tailwind.css";
+import { inject } from "@vercel/analytics";
+
+const App = () => {
+  if (Platform.OS === "web") {
+    inject();
+  }
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default App;
