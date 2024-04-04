542e04e23fe18b47eeff67dafeb1caa8af68648a
Ev357 <evest.dev@gmail.com>
2024-04-04 23:48:05 +0200

    build: i18n

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,13 @@ import { Platform } from "react-native";
 import { Redirect } from "expo-router";
 import { inject } from "@vercel/analytics";
 import "@/assets/css/tailwind.css";
+import "@/helpers/i18n";
 
 const App = () => {
   if (Platform.OS === "web") {
     inject();
   }
+
   return <Redirect href="/(tabs)/chats" />;
 };
 
